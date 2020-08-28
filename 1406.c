590d11e8541932d1d413df2e1cc8154e0a76ece2
kelly2657 <38282240+kelly2657@users.noreply.github.com>
2020-08-28 17:11:39 +0900

    Add files via upload

---
+++ 1406.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	char a[20];
+	char b[20];
+	strcpy(a,"love");
+	scanf("%s",b);
+	if(strcmp(a,b)==0){
+		printf("I love you.");
+	}
+	else{
+	}
+
+}
---
+++ 1743.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	char a[10];
+	scanf("%s",&a);
+	printf("welcome! %s",a);
+}
